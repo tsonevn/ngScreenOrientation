4df4843052bb62c63457b98481ebde2f91fe50d4
Nikolay Tsonev <nikolay.tsonev@telerik.com>
2016-08-19 10:31:46 +0300

    add roject

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+// this import should be first in order to load some required settings (like globals and reflect-metadata)
+import {nativeScriptBootstrap} from "nativescript-angular/application";
+import {AppComponent} from "./app.component";
+var application = require('application');
+require( "nativescript-orientation" );
+
+nativeScriptBootstrap(AppComponent);
